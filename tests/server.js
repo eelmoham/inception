e6958088d0b1e7993bf2eb49dca313fd3305979c
El mehdi Elmohammady <eelmoham@e2r12p4.1337.ma>
2023-05-08 23:51:44 +0100

    add more files

--- tests/server.js
+++ tests/server.js
@@ -4,6 +4,6 @@ const PORT   = 8080
 const app = express()
 
 app.get('/', (req,res) => res.send('<h1> looool !</h1>'))
-app.get('/home', (req,res) => res.send('<h1> Home</h1>'))
+app.get('/home', (req,res) => res.send('<h1> home page </h1>'))
 
 app.listen(PORT, () => console.log(`port ${PORT} is opening`))
