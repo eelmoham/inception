62d19f9cbf7c0234d3a21c208abe030cb5a25ec2
El mehdi Elmohammady <eelmoham@e2r13p4.1337.ma>
2023-05-11 01:53:30 +0100

    done

--- tests/src/server.js
+++ tests/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 
-const PORT   = 8080
+const PORT   = process.env.PORT || 8080
 const app = express()
+
+console.log(`loool ${process.env.user}`)
 
 app.get('/', (req,res) => res.send('<h1> its me!</h1>'))
 app.get('/home', (req,res) => res.send('<h1> home page </h1>'))
