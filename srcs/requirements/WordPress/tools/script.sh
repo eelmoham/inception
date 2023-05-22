2466bb506111381a3ab38a46c2b48246c03702ae
El mehdi Elmohammady <eelmoham@e2r13p2.1337.ma>
2023-05-22 02:45:07 +0100

    done

--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -24,6 +24,12 @@ chmod +x /usr/local/bin/wp
 mkdir -p /usr/share/nginx/html
 cd /usr/share/nginx/html && wp core download --allow-root #download the WordPress core files into the current directory
 touch /usr/share/nginx/html/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/g" /usr/share/nginx/html/wp-config-sample.php
+sed -i "s/username_here/$DB_USER/g" /usr/share/nginx/html/wp-config-sample.php
+sed -i "s/password_here/$DB_PSSWRD/g" /usr/share/nginx/html/wp-config-sample.php
+sed -i "s/localhost/$DB_HOST/g" /usr/share/nginx/html/wp-config-sample.php
+cd /usr/share/nginx/html/
+cp wp-config-sample.php wp-config.php
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PSSWRD --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PSSWRD --allow-root
 
-
-# exec "$@"
