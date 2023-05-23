3c6e52f3a5004c69261fec9e9e7ed56d9d334abf
El mehdi Elmohammady <eelmoham@e2r13p2.1337.ma>
2023-05-23 01:35:07 +0100

    done

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+##################################################
+DOMAIN_NAME=localhost    #eelmoham.42.fr        #
+## certificates                                 #
+# CERTS_=./XXXXXXXXXXXX                         #
+## MYSQL SETUP                                  #
+MYSQL_ROOT_PASSWORD=Mehdi@zero                  #
+MYSQL_USER=eelmoham                             #
+MYSQL_PASSWORD=Mehdi@zero                       #
+#################################################
+service mysql start # Start mysql
 
-service mysql start
-
-mysql -e "CREATE USER '$MYSQL_USER'@'$DOMAIN_NAME' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'$DOMAIN_NAME';"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'" # Change root password
+mysql -e "CREATE USER '$MYSQL_USER'@'$DOMAIN_NAME' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" # Create user #still not working
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'$DOMAIN_NAME';" # Grant privileges
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'" # Change root password
 mysql -e "FLUSH PRIVILEGES;"
 
-kill "cat /var/run/mysqld/mysqld.pid"
-
-exec "$@"
+kill $(cat /var/run/mysqld/mysqld.pid) # Stop mysql
--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -6,30 +6,45 @@ DB_USER="xxxx"
 DB_HOST="mariadb"
 
 WP_URL="localhost"
-WP_TITLE="website title"
+WP_TITLE="title"
 WP_ADMIN="admin"
 WP_ADMIN_PSSWRD="admin"
-WP_ADMIN_EMAIL="admin@alaoui.com"
+WP_ADMIN_EMAIL="admin@eelmoham.com"
 WP_USER="user"
 WP_USER_PSSWRD="user"
 WP_USER_EMAIL="user@eelmoham.com"
 
 
 apt-get clean
-mkdir -p /run/php
-mkdir -p /var/www/html
-sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
-wget -qO- https://wordpress.org/latest.tar.gz | tar -xz -C /var/www/html
-chmod +x /usr/local/bin/wp
-mkdir -p /usr/share/nginx/html
-cd /usr/share/nginx/html && wp core download --allow-root #download the WordPress core files into the current directory
-touch /usr/share/nginx/html/wp-config.php
-sed -i "s/database_name_here/$DB_NAME/g" /usr/share/nginx/html/wp-config-sample.php
-sed -i "s/username_here/$DB_USER/g" /usr/share/nginx/html/wp-config-sample.php
-sed -i "s/password_here/$DB_PSSWRD/g" /usr/share/nginx/html/wp-config-sample.php
-sed -i "s/localhost/$DB_HOST/g" /usr/share/nginx/html/wp-config-sample.php
-cd /usr/share/nginx/html/
-cp wp-config-sample.php wp-config.php
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PSSWRD --admin_email=$WP_ADMIN_EMAIL --allow-root
-wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PSSWRD --allow-root
 
+mkdir -p /run/php
+
+mkdir -p /var/www/html
+
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
+
+chmod +x /usr/local/bin/wp
+
+mkdir -p /usr/share/nginx/html
+
+cd /usr/share/nginx/html && wp core download --allow-root #download the WordPress core files into the current directory
+
+touch /usr/share/nginx/html/wp-config.php
+
+sed -i "s/database_name_here/$DB_NAME/g" /usr/share/nginx/html/wp-config-sample.php
+
+sed -i "s/username_here/$DB_USER/g" /usr/share/nginx/html/wp-config-sample.php
+
+sed -i "s/password_here/$DB_PSSWRD/g" /usr/share/nginx/html/wp-config-sample.php
+
+sed -i "s/localhost/$DB_HOST/g" /usr/share/nginx/html/wp-config-sample.php
+
+cd /usr/share/nginx/html/
+
+cp wp-config-sample.php wp-config.php
+
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PSSWRD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PSSWRD --allow-root
