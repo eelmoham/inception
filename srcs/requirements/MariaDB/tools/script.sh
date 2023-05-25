2a3f66d44e78d8ca253b40432e74bd3169f66c17
El mehdi Elmohammady <eelmoham@e2r12p4.1337.ma>
2023-05-25 01:53:07 +0100

    database connection done

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-sed -i 's/bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-
+# sed -i 's/bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+# sed -i "s/#port*/port = 3306" /etc/mysql/mariadb.conf.d/50-server.cnf
 service mysql start # Start mysql
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME" # Create database
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" # Create user #still not working
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'" # Grant privileges
-mysql -e "FLUSH PRIVILEGES"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"# Change root password
-mysql -e "FLUSH PRIVILEGES"
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD << EOF
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
 kill `cat /var/run/mysqld/mysqld.pid` # Stop mysql
+
+exec "$@" # Run CMD
+
+
+
+
+# mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
+# mysql -e "FLUSH PRIVILEGES;"
+# #!/bin/bash
+# mysql -u root -p"$MYSQL_ROOT_PASSWORD" < /path/to/setup.sql
--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -3,11 +3,15 @@
 apt-get clean
 mkdir -p /run/php
 sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
-sed -i "s/#port = 3306/port = 3306" "/etc/php/7.3/fpm/pool.d/www.conf"
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
 chmod +x /usr/local/bin/wp
-mkdir -p /usr/share/nginx/html #create shared directory for WordPress if it doesn't exist
-cd /usr/share/nginx/html && wp core download --allow-root #download the WordPress core files into the current directory
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PSSWRD --admin_email=$WP_ADMIN_EMAIL --allow-root
+mkdir -p /var/www/html #create shared directory for WordPress if it doesn't exist
+cd /var/www/html
+wp core download --allow-root #download the WordPress core files into the current directory
+cd /var/www/html
+#touch wp-config.php #create wp-config.php file
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PSSWRD --dbhost=$DB_HOST --allow-root
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PSSWRD --admin_email=$WP_ADMIN_EMAIL --allow-root
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PSSWRD --allow-root
+#cp wp-config-sample.php wp-config.php
+exec "$@"
