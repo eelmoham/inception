65f3bdeffdf5edb23bdf99180e5e2fbfbd356df9
El mehdi Elmohammady <eelmoham@e2r13p3.1337.ma>
2023-05-26 11:52:40 +0100

    great ,root and user eelmoham has same acces in mariadb

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
 
-sed -i 's/bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+# sed -i 's/bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 # sed -i "s/#port*/port = 3306" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start # Start mysql
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME" # Create database
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" # Create user #still not working
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION"
 
-mysql -uroot -p$MYSQL_ROOT_PASSWORD << EOF
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-FLUSH PRIVILEGES;
-EOF
-
-kill `cat /var/run/mysqld/mysqld.pid`
-
-exec "$@"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -e "FLUSH PRIVILEGES;"
 
 
-
-
-
-
-
-
-
-
-# service mysql start # Start mysql
-# mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME" # Create database
-# mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" # Create user #still not working
 
 # mysql -uroot -p$MYSQL_ROOT_PASSWORD << EOF
 # GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
@@ -38,14 +22,9 @@ exec "$@"
 # FLUSH PRIVILEGES;
 # EOF
 
-# kill $(cat /var/run/mysqld/mysqld.pid)
-
-exec "$@" # Run CMD
+kill `cat /var/run/mysqld/mysqld.pid`
 
 
+sed -i '/^\[mysqld\]/a skip-grant-tables' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-
-# mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
-# mysql -e "FLUSH PRIVILEGES;"
-# #!/bin/bash
-# mysql -u root -p"$MYSQL_ROOT_PASSWORD" < /path/to/setup.sql
+exec "$@"
