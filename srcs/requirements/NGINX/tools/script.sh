535b7df778238dc7f973539150d07e141f13bd4e
El mehdi Elmohammady <eelmoham@e2r13p4.1337.ma>
2023-05-29 10:02:20 +0100

    done with no error2

--- srcs/requirements/NGINX/tools/script.sh
+++ srcs/requirements/NGINX/tools/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/nginx-selfsigned.key -out /etc/nginx/certs/nginx-selfsigned.crt -subj "/C=US/ST=California/L=San Francisco/O=My Company/OU=DevOps/CN=$DOMAIN_NAME"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/nginx-selfsigned.key -out $CERTS_ -subj "/C=US/ST=California/L=San Francisco/O=My Company/OU=DevOps/CN=$DOMAIN_NAME"
  
